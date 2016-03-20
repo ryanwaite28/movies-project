187072c76fd901ff8f5f459ce605d53d4d9b90f5
Ryan M. Waite <ryanwaite28@gmail.com>
2016-03-19 23:17:24 -0400

    Update builder.py

--- builder.py
+++ builder.py
@@ -8,7 +8,8 @@ main_page_head = '''
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
     <title>Fresh Tomatoes!</title>
 
     <!-- Bootstrap 3 -->
